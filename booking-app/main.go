ee8a54a04fbdb71b4c2536774dfe6673a3e43a94
arunraj3 <arunrajgovindan2002@gmail.com>
2023-07-14 02:05:05 +0530

    done

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,10 +3,19 @@ import "fmt"
 
 func main(){
 	var conferenceName  = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conferenceName is of Type : %T\n",conferenceName)
 	fmt.Println("Welcome to our",conferenceName,"Booking Application")
-	
+	fmt.Printf("We have total of %d tickets and %d are still available\n",conferenceTickets,remainingTickets)
+
+	var userName string
+	var userTickets int 
+
 
 	
-	
-
+	userName = "arun"
+	userTickets = 2
+	fmt.Printf("User %s booked %d tickets",userName,userTickets)
 }
