50bb73ac0fb19fea81a8ffa58bd41176fc715b52
arunraj3 <arunrajgovindan2002@gmail.com>
2023-07-19 12:50:52 +0530

    new helper package

---
+++ booking-app/helper.go
@@ -0,0 +1,12 @@
+package main
+
+
+import "strings"
+
+
+func validateUserInput(firstName string, lastName string, userTickets uint, email string) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+	return isValidName, isValidEmail, isValidTicketNumber
+}
